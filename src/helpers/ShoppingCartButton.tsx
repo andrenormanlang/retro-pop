4e15fee903bcaf182b2e815dffcea2716d041998
andrenormanlang <andrenormanlang@gmail.com>
2025-02-22 20:49:15 +0100

    fixed supermarket cart button nested within button error

--- src/helpers/ShoppingCartButton.tsx
+++ src/helpers/ShoppingCartButton.tsx
@@ -1,29 +1,6 @@
-import { Box, IconButton } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/react";
 import { FiShoppingCart } from "react-icons/fi";
 
-const ShoppingCartButton = () => (
-  <Box>
-    <IconButton
-      aria-label="Cart"
-      icon={<FiShoppingCart />}
-      size="md"
-      variant="outline"
-      colorScheme="blue"
-      _hover={{
-        bg: "blue.500",
-        color: "white",
-        borderColor: "blue.500",
-      }}
-      _active={{
-        bg: "blue.600",
-        color: "white",
-        borderColor: "blue.600",
-      }}
-      _focus={{
-        boxShadow: "outline",
-      }}
-    />
-  </Box>
-);
+const ShoppingCartButton = () => <Icon as={FiShoppingCart} boxSize={6} />;
 
 export default ShoppingCartButton;
