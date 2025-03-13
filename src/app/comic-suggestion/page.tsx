075998d27108c4a5551e48d42bdf0e53b7234b0f
andrenormanlang <andrenormanlang@gmail.com>
2025-03-13 20:20:40 +0100

    added suspense in comics-suggestion for deployment

--- src/app/comic-suggestion/page.tsx
+++ src/app/comic-suggestion/page.tsx
@@ -1,11 +1,19 @@
-"use client"
+"use client";
 
+import React, { Suspense } from "react";
+import { Center, Spinner } from "@chakra-ui/react";
 import ComicSuggestion from "./_components/ComicSuggestion.client";
 
 export default function ComicSuggestionPage() {
 	return (
-		<div>
+		<Suspense
+			fallback={
+				<Center h="100vh">
+					<Spinner size="xl" />
+				</Center>
+			}
+		>
 			<ComicSuggestion />
-		</div>
+		</Suspense>
 	);
 }
