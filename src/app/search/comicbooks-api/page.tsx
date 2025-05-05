ae5af0f65e765c9dcf523368be32c1615a3e02b5
andrenormanlang <andrenormanlang@gmail.com>
2025-05-05 09:29:06 +0200

    adding more links for get comics

--- src/app/search/comicbooks-api/page.tsx
+++ src/app/search/comicbooks-api/page.tsx
@@ -15,199 +15,201 @@ import { useGetComicBooksApi } from "@/hooks/comicbooks-api/useGetComicBooksAPI"
 import { ComicBooksAPI } from "@/types/cbAPI.types";
 
 const CBAPI: NextPage = () => {
-  const pageSize = 1;
-  const { searchTerm, setSearchTerm, currentPage, setCurrentPage, updateUrl } = useSearchParameters(1, "");
+	const pageSize = 1;
+	const { searchTerm, setSearchTerm, currentPage, setCurrentPage, updateUrl } = useSearchParameters(1, "");
 
-  const [searchQuery, setSearchQuery] = useState<SearchQuery>({
-    page: 1,
-    query: "",
-  });
+	const [searchQuery, setSearchQuery] = useState<SearchQuery>({
+		page: 1,
+		query: "",
+	});
 
-  const getColorScheme = (serviceName: string): string => {
-    const schemes: { [key: string]: string } = {
-      DOWNLOADNOW: "blue",
-      TERABOX: "green",
-      PIXELDRAIN: "orange",
-      MEDIAFIRE: "red",
-      READONLINE: "purple",
-      // Add other services and their corresponding color schemes
-    };
-    return schemes[serviceName] || "gray"; // Default color scheme if not found
-  };
+	const getColorScheme = (serviceName: string): string => {
+		const schemes: { [key: string]: string } = {
+			DOWNLOADNOW: "blue",
+			MEGA: "teal",
+			VIKINGBOX:"yellow",
+			TERABOX: "green",
+			PIXELDRAIN: "orange",
+			MEDIAFIRE: "red",
+			READONLINE: "purple",
 
-  const desiredButtons = ["DOWNLOADNOW", "MEDIAFIRE", "READONLINE"];
+			// Add other services and their corresponding color schemes
+		};
+		return schemes[serviceName] || "gray"; // Default color scheme if not found
+	};
 
-  const router = useRouter();
+	const desiredButtons = ["DOWNLOADNOW", "MEGA", "MEDIAFIRE", "VIKINGBOX", "TERABOX", "PIXELDRAIN", "READONLINE"];
 
-  const { data, isLoading, isError, error } = useGetComicBooksApi(searchTerm, currentPage, pageSize);
+	const router = useRouter();
 
-  const handleSearchTerm = useDebouncedCallback((term: string) => {
-    setSearchTerm(term);
-    setCurrentPage(1);
-    updateUrl(term, 1);
-    setSearchQuery({ ...searchQuery, query: term, page: 1 });
-  }, 300);
+	const { data, isLoading, isError, error } = useGetComicBooksApi(searchTerm, currentPage, pageSize);
 
-  useEffect(() => {
-    const url = new URL(window.location.href);
-    url.searchParams.set("page", currentPage.toString());
-    if (searchTerm) {
-      url.searchParams.set("query", searchTerm);
-    } else {
-      url.searchParams.delete("query");
-    }
-    // Convert URL object to a string
-    const urlString = url.toString();
-    router.push(urlString, undefined);
-  }, [searchTerm, currentPage, router]);
+	const handleSearchTerm = useDebouncedCallback((term: string) => {
+		setSearchTerm(term);
+		setCurrentPage(1);
+		updateUrl(term, 1);
+		setSearchQuery({ ...searchQuery, query: term, page: 1 });
+	}, 300);
 
-  if (isLoading) {
-    return (
-      <Center h="100vh">
-        <Spinner size="xl" />
-      </Center>
-    );
-  }
+	useEffect(() => {
+		const url = new URL(window.location.href);
+		url.searchParams.set("page", currentPage.toString());
+		if (searchTerm) {
+			url.searchParams.set("query", searchTerm);
+		} else {
+			url.searchParams.delete("query");
+		}
+		// Convert URL object to a string
+		const urlString = url.toString();
+		router.push(urlString, undefined);
+	}, [searchTerm, currentPage, router]);
 
-  if (isError) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh", // Full viewport height
-          fontFamily: '"Bangers", cursive', // Assuming "Bangers" font is loaded
-          fontSize: "1.5rem", // Larger font size
-          color: "red", // Red color for the error message
-          textAlign: "center",
-          padding: "20px",
-          backgroundColor: "#f0f0f0", // Light background for visibility
-          borderRadius: "10px",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)", // Optional shadow for better appearance
-        }}
-      >
-        Error: {error.message}
-      </div>
-    );
-  }
+	if (isLoading) {
+		return (
+			<Center h="100vh">
+				<Spinner size="xl" />
+			</Center>
+		);
+	}
 
-  console.log("data", data);
+	if (isError) {
+		return (
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					height: "100vh", // Full viewport height
+					fontFamily: '"Bangers", cursive', // Assuming "Bangers" font is loaded
+					fontSize: "1.5rem", // Larger font size
+					color: "red", // Red color for the error message
+					textAlign: "center",
+					padding: "20px",
+					backgroundColor: "#f0f0f0", // Light background for visibility
+					borderRadius: "10px",
+					boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)", // Optional shadow for better appearance
+				}}
+			>
+				Error: {error.message}
+			</div>
+		);
+	}
 
-  return (
-    <Suspense
-      fallback={
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh", // Full viewport height
-            fontFamily: '"Bangers", cursive', // Assuming "Bangers" font is loaded
-            fontSize: "1.5rem", // Larger font size
-            color: "red", // Red color for the error message
-            textAlign: "center",
-            padding: "20px",
-            backgroundColor: "#f0f0f0", // Light background for visibility
-            borderRadius: "10px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)", // Optional shadow for better appearance
-          }}
-        >
-          Loading...
-        </div>
-      }
-    >
-      <Container maxW="container.xl" centerContent p={4}>
-        <SearchBox onSearch={handleSearchTerm} />
-        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} width="100%">
-          {data?.map((comic: ComicBooksAPI, index: number) => {
-            return (
-              <motion.div
-                key={`${comic.id}-${index}`} // Ensures unique key
-                whileHover={{ scale: 1.05 }}
-                style={{
-                  textDecoration: "none",
-                  cursor: "pointer",
-                }}
-              >
-                <Box
-                  boxShadow="0 4px 8px rgba(0,0,0,0.1)"
-                  rounded="md"
-                  overflow="hidden"
-                  p={4}
-                  display="flex"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  minH="630px"
-                  position="relative"
-                >
-                  <Image
-                    src={comic.coverPage}
-                    alt={comic.title || `Comic ${comic.title}`}
-                    maxW="400px"
-                    maxH="400px"
-                    objectFit="contain"
-                  />
-                  <Text fontWeight="bold" fontSize="md" textAlign="center" mt={4}>
-                    {comic.title}
-                  </Text>
-                  <Text fontSize="sm" color="gray.500" textAlign="left" mt={1} p={2}>
-                    {comic.description}
-                  </Text>
-                  <VStack spacing={4} align="stretch" mt={"1rem"}>
-                    {Object.entries(comic.downloadLinks)
-                      .filter(([key]) => desiredButtons.includes(key))
-                      .map(([key, value], btnIndex) => (
-                        <Button
-                          as="a"
-                          href={value}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          key={`${comic.id}-${key}-${btnIndex}`} // Ensures unique key
-                          leftIcon={<DownloadIcon />}
-                          colorScheme={getColorScheme(key)}
-                          _hover={{ transform: "scale(1.05)" }}
-                          aria-label={`Download from ${key}`}
-                          w="full"
-                        >
-                          {key}
-                        </Button>
-                      ))}
-                  </VStack>
-                </Box>
-              </motion.div>
-            );
-          })}
-        </SimpleGrid>
-        <div style={{ marginTop: "2rem", display: "flex", justifyContent: "space-between" }}>
-          <Button
-            onClick={() => setCurrentPage(1)}
-            disabled={currentPage === 1}
-            style={{ marginRight: "1rem" }}
-          >
-            First
-          </Button>
+	console.log("data", data);
 
-          <Button
-            onClick={() => setCurrentPage(currentPage - 1)}
-            style={{ marginLeft: "1rem", marginRight: "1rem" }}
-            disabled={currentPage === 1} // Prevent going below page 1
-          >
-            Previous
-          </Button>
+	return (
+		<Suspense
+			fallback={
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						height: "100vh", // Full viewport height
+						fontFamily: '"Bangers", cursive', // Assuming "Bangers" font is loaded
+						fontSize: "1.5rem", // Larger font size
+						color: "red", // Red color for the error message
+						textAlign: "center",
+						padding: "20px",
+						backgroundColor: "#f0f0f0", // Light background for visibility
+						borderRadius: "10px",
+						boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)", // Optional shadow for better appearance
+					}}
+				>
+					Loading...
+				</div>
+			}
+		>
+			<Container maxW="container.xl" centerContent p={4}>
+				<SearchBox onSearch={handleSearchTerm} />
+				<SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} width="100%">
+					{data?.map((comic: ComicBooksAPI, index: number) => {
+						return (
+							<motion.div
+								key={`${comic.id}-${index}`} // Ensures unique key
+								whileHover={{ scale: 1.05 }}
+								style={{
+									textDecoration: "none",
+									cursor: "pointer",
+								}}
+							>
+								<Box
+									boxShadow="0 4px 8px rgba(0,0,0,0.1)"
+									rounded="md"
+									overflow="hidden"
+									p={4}
+									display="flex"
+									flexDirection="column"
+									alignItems="center"
+									justifyContent="space-between"
+									minH="630px"
+									position="relative"
+								>
+									<Image
+										src={comic.coverPage}
+										alt={comic.title || `Comic ${comic.title}`}
+										maxW="400px"
+										maxH="400px"
+										objectFit="contain"
+									/>
+									<Text fontWeight="bold" fontSize="md" textAlign="center" mt={4}>
+										{comic.title}
+									</Text>
+									<Text fontSize="sm" color="gray.500" textAlign="left" mt={1} p={2}>
+										{comic.description}
+									</Text>
+									<VStack spacing={4} align="stretch" mt={"1rem"}>
+										{Object.entries(comic.downloadLinks)
+											.filter(([key]) => desiredButtons.includes(key))
+											.map(([key, value], btnIndex) => (
+												<Button
+													as="a"
+													href={value}
+													target="_blank"
+													rel="noopener noreferrer"
+													key={`${comic.id}-${key}-${btnIndex}`} // Ensures unique key
+													leftIcon={<DownloadIcon />}
+													colorScheme={getColorScheme(key)}
+													_hover={{ transform: "scale(1.05)" }}
+													aria-label={`Download from ${key}`}
+													w="full"
+												>
+													{key}
+												</Button>
+											))}
+									</VStack>
+								</Box>
+							</motion.div>
+						);
+					})}
+				</SimpleGrid>
+				<div style={{ marginTop: "2rem", display: "flex", justifyContent: "space-between" }}>
+					<Button
+						onClick={() => setCurrentPage(1)}
+						disabled={currentPage === 1}
+						style={{ marginRight: "1rem" }}
+					>
+						First
+					</Button>
 
-          <Button onClick={() => setCurrentPage(currentPage + 1)} style={{ marginLeft: "1rem" }}>
-            Next
-          </Button>
-        </div>
-      </Container>
-    </Suspense>
-  );
+					<Button
+						onClick={() => setCurrentPage(currentPage - 1)}
+						style={{ marginLeft: "1rem", marginRight: "1rem" }}
+						disabled={currentPage === 1} // Prevent going below page 1
+					>
+						Previous
+					</Button>
+
+					<Button onClick={() => setCurrentPage(currentPage + 1)} style={{ marginLeft: "1rem" }}>
+						Next
+					</Button>
+				</div>
+			</Container>
+		</Suspense>
+	);
 };
 
 export default CBAPI;
-
 
 // "use client";
 
