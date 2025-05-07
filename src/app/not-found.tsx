bb76ed1ece10b4003bbc4675c449f9445bf75510
andrenormanlang <andrenormanlang@gmail.com>
2025-05-07 13:03:04 +0200

    fixing page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,18 @@ import { Box, Heading, Text, Button, Center, VStack } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function NotFound() {
-  return (
-    <Center h="100vh">
-      <VStack spacing={6}>
-        <Heading as="h1" size="2xl">404</Heading>
-        <Text fontSize="xl">Page Not Found</Text>
-        <Text>We couldn&apos;t find the page you were looking for.</Text>
-        <Link href="/" passHref>
-          <Button colorScheme="blue">Return Home</Button>
-        </Link>
-      </VStack>
-    </Center>
-  );
+	return (
+		<Center h="100vh">
+			<VStack spacing={6}>
+				<Heading as="h1" size="2xl">
+					404
+				</Heading>
+				<Text fontSize="xl">Page Not Found</Text>
+				<Text>We couldn&apos;t find the page you were looking for.</Text>
+				<Link href="/" passHref>
+					<Button colorScheme="blue">Return Home</Button>
+				</Link>
+			</VStack>
+		</Center>
+	);
 }
